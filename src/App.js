c98f87311b51ec1f6531c39d7202da4389e0af19
beatrizmoreira <beatriz.moreira@ifce.edu.br>
2025-11-03 11:32:17 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Banner from './Componentes/Banner';
+import Formulario from './Componentes/Formulario';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componentes/Formulario/index.js
@@ -0,0 +1,18 @@
+import CampoTexto from "../CampoTexto"
+import './formulario.css'
+
+function Formulario() {
+    return (
+        <section className="formulario">
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                
+                <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
+                <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
