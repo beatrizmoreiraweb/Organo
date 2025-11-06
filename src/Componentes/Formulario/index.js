d19d61fec64f36d7db44b77087bba1148d11510b
beatrizmoreira <beatriz.moreira@ifce.edu.br>
2025-11-06 18:05:31 -0300

    Adição da Lista Suspensa

--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -1,7 +1,18 @@
 import CampoTexto from "../CampoTexto"
+import ListaSuspensa from "../ListaSuspensa"
 import './formulario.css'
 
+
 function Formulario() {
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
     return (
         <section className="formulario">
             <form>
@@ -10,6 +21,7 @@ function Formulario() {
                 <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
                 <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <ListaSuspensa label="Time" itens={times}/>
             </form>
         </section>
     )
