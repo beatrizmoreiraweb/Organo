68c9c155d719e249bd4a776425ef71e9b3d963f2
beatrizmoreira <beatriz.moreira@ifce.edu.br>
2025-11-10 13:01:58 -0300

    Lista realizada

--- src/Componentes/ListaSuspensa/index.js
+++ src/Componentes/ListaSuspensa/index.js
@@ -5,7 +5,7 @@ function ListaSuspensa(props) {
         <div className='lista-suspensa'>
             <label>{props.label}</label>
 
-            <select>
+            <select required={props.obrigatorio}>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
