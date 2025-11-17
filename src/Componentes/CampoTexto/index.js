80853b3ab25494f42dac2c20640ea54510cc05d9
beatrizmoreira <beatriz.moreira@ifce.edu.br>
2025-11-17 13:11:25 -0300

    Primeiro uso de hooks em processo

--- src/Componentes/CampoTexto/index.js
+++ src/Componentes/CampoTexto/index.js
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 function CampoTexto(props) {
+    //let valor = 'Guilherme Silveira'
+    const [valor, setValor]= useState('')
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
     return (
         <div className='campo-texto'>
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
